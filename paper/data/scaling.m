1af88749cf93d9d06e8a79c79c5de95c88a5f282
Ed Bueler <elbueler@alaska.edu>
2023-01-21 19:49:59 -0900

    clean up script used in paper ... realize I probably mis-read Isaac ... actually alpha=0.07?

--- paper/data/scaling.m
+++ paper/data/scaling.m
@@ -1,4 +1,4 @@
-% generate serial scaling for bueler 2016 and isaac et al 2015 and tumarino et al 2016
+% generate complexity data for bueler 2016 and isaac et al 2015 and tumarino et al 2016
 
 % note on iterations: if iterations scale as O(m^alpha) then flops scale as O(m^{1+alpha}); this is under assumption that each iteration costs O(m) flops
 
@@ -6,19 +6,28 @@
 % isaac: AMG-Krylov-Newton Stokes solve for antarctica; see table 8.1 in paper
 % tumarino: MDSC–AMG-Krylov-Newton higher-order solve for greenland; see table 7.5 in paper; area of Greenland taken from Searise data: 300x560 5km grid
 
+% BUELER
 nb = [18 36 72 144 288].^2;
 flopsb = [1.776e7 1.749e8 2.374e9 2.608e10 2.927e11];
-pb = polyfit(log(nb),log(flopsb),1);
+pb = polyfit(log(nb),log(flopsb),1)
 %ans = 1.76186   6.45957   ==>  beta = 0.76186
 
+% ISAAC:  publication uses kitsi.*nitsi (last), but probably 1st is correct
 ni = [7e6 51e6 383e6];
-itsi = [7*66 10*75 11*90]; % total Krylov iterations over Newton steps
-pi = polyfit(log(ni),log(itsi),1);
+kitsi = [66 75 90]; % total ("overall") Krylov iterations; averaged over P vals
+pi = polyfit(log(ni),log(kitsi),1)
+%ans = 0.0775   2.9589   ==>  alpha = 0.0775
+%flopsi = ni .* kitsi;
+%pflopsi = polyfit(log(ni),log(flopsi),1)
+%ans = 1.0775   2.9589  ==>  alpha = 0.0775  (same as above)
+%nitsi = [7 10 11]; % round down average Newton iterations to integer
+%pi = polyfit(log(ni),log(kitsi .* nitsi),1)
 %ans = 0.19030   3.17188   ==>  alpha = 0.19030
 
+% TUMINARO
 nt = ((300*560*5^2) / (8^2)) * [1 4 16 64 256];
 itst = [36*13.3 34*12.7 35*12.3 36*13.3 37*17.4]; % total Krylov iterations over Newton steps
-pt = polyfit(log(nt),log(itst),1);
+pt = polyfit(log(nt),log(itst),1)
 %ans = 0.05017   5.49297   ==>  alpha = 0.05017
 
 subplot(3,1,1)
@@ -32,7 +41,7 @@ hold off
 legend(sprintf('O(m^{%.3d})',pb(1)))
 
 subplot(3,1,2)
-loglog(ni, itsi, 'ro')
+loglog(ni, kitsi, 'ro')
 xlabel('degrees of freedom m')
 ylabel('total krylov iterations')
 title('Isaac')
